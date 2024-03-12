e8836d34e169e869a63070fc37fe2d950be12526
shybyjoin <140958756+shybyjoin@users.noreply.github.com>
2024-03-12 09:25:29 +0200

    Update uploadfile.py

--- uploadfile.py
+++ uploadfile.py
@@ -9,8 +9,8 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME,API_VERION,SCOPES)
 
-folder_id = '134Mw24lJQgDGG9Z3AIn0EzDmph3Ft6mc'
-file_names = ['real_hood.jpg']
+folder_id = '*folderid*'
+file_names = ['folder_name.jpg']
 mime_types = ['image/jpeg']
 
 for file_name, mime_type in zip(file_names, mime_types):
@@ -19,7 +19,7 @@ for file_name, mime_type in zip(file_names, mime_types):
         'parents': [folder_id]
     }
 
-    media = MediaFileUpload('./fotografii/{0}'.format(file_name), mimetype=mime_type, resumable= True)
+    media = MediaFileUpload('./partition/{0}'.format(file_name), mimetype=mime_type, resumable= True)
    
     service.files().create(
        body=file_metadata,
